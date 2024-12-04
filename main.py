d0d1185dcd00030c8e6b079efd15069911bd64b8
Shrekington <shrekthesaviour69@gmail.com>
2024-12-04 03:55:40 +0100

    Implement text analysis with word count and character frequencies

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def main():
         file_contents = f.read()
     num_words = count_words(file_contents)
     num_chars = count_chars(file_contents)
-    print(num_words)
-    print(num_chars)
+    
+    char_list = [{"char": key, "count": value} for key, value in num_chars.items()]
+    char_list.sort(reverse=True, key=sort_on)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in char_list:
+        if item["char"].isalpha():
+            print(f"The {item["char"]} character was found {item["count"]} times")
 
 def count_words(text):
     words = text.split()
@@ -22,5 +31,7 @@ def count_chars(text):
     
     return num_chars
 
+def sort_on(dict):
+    return dict["count"]
 
 main()
